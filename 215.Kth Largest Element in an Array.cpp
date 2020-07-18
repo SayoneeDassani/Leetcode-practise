b3c42b9d616e87f0352adf5a223cbab4626c5ff1
Sayonee <40688223+SayoneeDassani@users.noreply.github.com>
2020-07-18 08:22:11 +0530

    Create 215.Kth Largest Element in an Array.cpp

---
+++ 215.Kth Largest Element in an Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        map< int , int> m;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+            
+        }
+        
+        map<int, int> :: iterator it;
+        for(it=m.begin();it!=m.end();it++)
+        {
+            if (it->second > 1)
+                 return it->first;
+        }
+        return 0;
+    }
+};
